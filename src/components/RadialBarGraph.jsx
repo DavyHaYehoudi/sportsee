530da975f4bab9bcb7b0a8291915080ed1d36c6a
DavyHaYehoudi <davyhayehoudi@gmail.com>
2024-02-29 12:01:29 +0100

    radialBar graph

--- src/components/RadialBarGraph.jsx
+++ src/components/RadialBarGraph.jsx
@@ -1,54 +1,40 @@
 import React from "react";
-import {
-  RadialBarChart,
-  RadialBar,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
-import score from "../mocks/userMainData.json"
-const data = [
-  {
-    name: "18-24",
-    uv: 31.47,
-    pv: 2400,
-    fill: "#8884d8",
-  }
-];
-const style = {
-    top: '50%',
-    right: 0,
-    transform: 'translate(0, -50%)',
-    lineHeight: '24px',
-  };
+import { RadialBarChart, RadialBar, ResponsiveContainer } from "recharts";
+import data from "../mocks/userMainData.json";
+
 const RadialBarGraph = () => {
-    const scoreFormatted = [score.data.todayScore]
+  const { todayScore, score } = data.data;
+  const dataScore = todayScore ? todayScore : score;
+  const formattedData = [{ name: "score", value: dataScore }];
   return (
     <div className="block">
+      <h3 className="score-title">Score</h3>
       <ResponsiveContainer width="100%" height="100%">
         <RadialBarChart
-          cx="50%"
-          cy="50%"
-          innerRadius="10%"
-          outerRadius="80%"
-          barSize={10}
-        //   cornerRadius={10}
-          data={scoreFormatted}
+          outerRadius="0%"
+          data={formattedData}
+          startAngle={90}
+          endAngle={450}
         >
           <RadialBar
-            minAngle={15}
-            label={{ position: "insideStart", fill: "#fff" }}
-            background
-            clockWise
-            dataKey="uv"
+            data={[{ value: 1 }]}
+            dataKey="value"
+            barSize={170}
+            fill="var(--white)"
           />
-          <Legend
-            iconSize={10}
-            layout="vertical"
-            verticalAlign="middle"
-            wrapperStyle={style}
+          <RadialBar
+            dataKey="value"
+            barSize={10}
+            cornerRadius={100}
+            fill="var(--redRadialBar)"
           />
         </RadialBarChart>
       </ResponsiveContainer>
+      <div className="score-details-wrapper">
+        <span className="score-details-number">{dataScore * 100}%</span>
+        <span className="score-details-text">de votre</span>
+        <span className="score-details-text">objectif</span>
+      </div>
     </div>
   );
 };
